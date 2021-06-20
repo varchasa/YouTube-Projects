76172371a0a71979f6b8dcc3de54bbea07f20ca8
Varchasa Aggarwal <43700017+varchasa@users.noreply.github.com>
2021-06-20 18:04:13 +0530

    screen recorder

--- Screen Recoder/screen_recorder.py
+++ Screen Recoder/screen_recorder.py
@@ -11,19 +11,20 @@ def main ():
     screen_recorder.enable_dev_log ()
     
     params = screen_recorder.RecorderParams ()
-    # params.pid = 0 # use it to set process Id to capture
-    # params.desktop_num = 0 # use it to set desktop num, counting from 0
-     #intialize the screen recoder
+   
+    #intialize the screen recoder
     screen_recorder.init_resources (params)
+    
     #take screenshot and save it 
     screen_recorder.get_screenshot (5).save ('test_before.png')
+    
     #start video recording
     screen_recorder.start_video_recording ('video1.mp4', 30, 8000000, True)
+    
     #time limit 
     time.sleep (hours*60+seconds)
     
-    
-    #stp the recording
+    #stop the recording
     screen_recorder.stop_video_recording ()
 
 
